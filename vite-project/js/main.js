f93f338264241d7ffce6760c527feb46d6f6fff4
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2023-12-11 10:14:12 -0500

    www

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,19 @@
 import './style.css'
-const Url = 'https://api.waifu.im/search'
+const URL = 'https://api.waifu.im/search'
 
-async function getData(Url) {
+async function getData(URL) {
+    try{
+    const response = await fetch(URL);
+    const data = await response.json(); //makes data into json
+    console.log(data.content);
+    }
+
+}
+getData(URL);
+
+/* async function getData(Url) {
     try{const response = await fetch(url)
     const data = await response.json()
 console.log(data)
 }catch(error) {console.log(error);}
-}
+} */
